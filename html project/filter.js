3562672fc88a2db8abafc84094878dcf11da3839
ReiJ0908 <reinaldo.jaramillo@owl.ucc.edu>
2024-05-03 11:30:14 -0400

    huh

--- html project/filter.js
+++ html project/filter.js
@@ -208,6 +208,7 @@ const btns2=[
     
     const categories = [...new Set(product.map((item)=>
         {return item}))]
+        let i=0;
 
     
     const filterItems = (a)=>{
@@ -234,21 +235,61 @@ const btns2=[
         displayItem(flterCategories2)
     }
     
+    var cart =[];
+
+    function addtocart(a){
+      cart.push({...categories[a]});
+      displaycart();
+    }
+
+    function delElement(a){
+      cart.splice(a, 1);
+      displaycart();
+  }
+
+  function displaycart(){
+    let j = 0, total=0;
+    document.getElementById("count").innerHTML=cart.length;
+    if(cart.length==0){
+        document.getElementById('cartItem').innerHTML = "Your cart is empty";
+        document.getElementById("total").innerHTML = "$ "+0+".00";
+    }
+    else{
+        document.getElementById("cartItem").innerHTML = cart.map((items)=>
+        {
+            var {image, title, price} = items;
+            total=total+price;
+            document.getElementById("total").innerHTML = "$ "+total+".00";
+            return(
+                `<div class='cart-item'>
+                <div class='row-img'>
+                    <img class='rowimg' src=${image}>
+                </div>
+                <p style='font-size:12px;'>${title}</p>
+                <h2 style='font-size: 15px;'>$ ${price}.00</h2>`+
+                "<i class='fa-solid fa-trash' onclick='delElement("+ (j++) +")'></i></div>"
+            );
+        }).join('');
+    }
+
     
+}
+
     const displayItem = (items) => {
         document.getElementById('root').innerHTML = items.map((item)=>
         {
             var {image, title, price} = item;
             return(
                 `<div class='box'>
+                
                 <h3>${title}</h3>
                 <div class='img-box'>
                 <img class='images' src=${image}></img>
                 </div>
                 <div class='bottom'>
-                <h2>$ ${price}.00</h2>
-                <button>Add to cart</button>
-                </div>
+                <h2>$ ${price}.00</h2> `+
+                "<button onclick='addtocart("+(i++)+")'>Add to cart</button>"+
+                `</div>
                 </div>`)
         }).join('');
     }
